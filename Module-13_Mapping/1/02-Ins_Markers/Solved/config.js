b07d6475e3c942fe21b5f4db019fff957250b3de
edkrueger <edkrueger@gmail.com>
2021-11-08 21:05:06 -0600

    Add day 1  leaflet solutions.

---
+++ Module-13_Mapping/1/02-Ins_Markers/Solved/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZWRrcnVlZ2VyIiwiYSI6ImNrdnJjc2J5c2F5OGMydnBnOHRneWk1YWIifQ.UrM1mTtbjECbRBBp-xoB6Q"
---
+++ Module-13_Mapping/1/04-Ins_City_Population/Solved/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZWRrcnVlZ2VyIiwiYSI6ImNrdnJobzJ1eDJ2ZWQyd3FnNXN2MmJxNjMifQ.LoYKrkO65BaFIqjdrpByTA";
